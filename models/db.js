5b87a0a5687c7a2f792fa967ce3ff7158c448aa7
FillipeW <welausen@gmail.com>
2023-10-31 18:03:50 -0300

    update-31-10-23

--- models/db.js
+++ models/db.js
@@ -6,10 +6,10 @@ const Sequelize = require('sequelize');
 // })
 
 
-const sequelize = new Sequelize('dbtest','admin','Lipe2711',{
-    host:'pacotama-db1.cvk7zq60hyhi.us-east-1.rds.amazonaws.com',
+const sequelize = new Sequelize('railway','root','iSceV4Y9ddj2hYP92RF9',{
+    host:'containers-us-west-78.railway.app',
     dialect:'mysql',
-    port:3306
+    port:6099
 })
 
 
